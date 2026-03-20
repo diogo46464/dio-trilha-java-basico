70014457d79d1f93acbeb9d157c982028c943e15
Diogo Pincetta <diogohe08@gmail.com>
2026-03-20 16:13:49 -0300

    Foi criado tipos de variáveis

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,24 @@
 public class TipoVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+
+        //Tipos primitivos 
+        
+        String meuNome = "Diogo Henrique";
+
+        double salarioMinimo = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        long numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        
+
     }
 }
